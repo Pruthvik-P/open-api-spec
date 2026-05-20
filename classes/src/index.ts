52516a88a5761aceacb952b8163e2de097c4224b
pruthvi-hash <pruthvik@lokveeraj.com>
2026-05-20 22:26:33 +0530

    day1

---
+++ classes/src/index.ts
@@ -0,0 +1,23 @@
+import {measure, once} from "helpful-decorators";
+
+class DateClass {
+  private timeZone: string;
+  constructor(timeZone: string) {
+    this.timeZone = timeZone;
+  }
+
+  @measure
+  getTime() {
+    var d = new Date();
+    console.log("hi grom getTime")
+    return d.getTime();
+  }
+
+}
+
+const dateObject = new DateClass("IND");
+dateObject.getTime();
+dateObject.getTime();
+dateObject.getTime();
+dateObject.getTime();
+dateObject.getTime();
